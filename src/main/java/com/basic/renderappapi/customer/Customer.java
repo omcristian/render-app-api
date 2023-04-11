5f32c3826fe4a5cc5537564c735e8f1fd60e1bb1
Cristian Ortiz Muñoz <cristian.ortiz@overactive.com>
2023-04-10 23:49:18 -0400

    Initial commit

---
+++ src/main/java/com/basic/renderappapi/customer/Customer.java
@@ -0,0 +1,12 @@
+package com.basic.renderappapi.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ src/test/java/com/basic/renderappapi/RenderAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.renderappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
